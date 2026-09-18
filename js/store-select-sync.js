ecc1baa83d986b24a1fab4f164fba278ff43dea8
petromosi-pixel <petromosi@gmail.com>
2026-09-18 18:53:30 +0330

    fix(v6): restore simulator scroll asset version

--- js/store-select-sync.js
+++ js/store-select-sync.js
@@ -25,7 +25,7 @@
       return;
     }
     var script=document.createElement('script');
-    script.src='js/v6-store-browser.js?v=6.0.0-store-browser.8';
+    script.src='js/v6-store-browser.js?v=6.0.0-store-browser.9';
     script.async=false;
     script.dataset.digiyarStoreBrowser='1';
     script.onload=function(){ callback(window.DigiYarStoreBrowser); };
