093e1aa7218871245b2a6f4b0790ca22688bc0aa
petromosi-pixel <petromosi@gmail.com>
2026-09-23 22:38:19 +0330

    V7: bust store browser cache for live adapters

--- js/store-select-sync.js
+++ js/store-select-sync.js
@@ -25,7 +25,7 @@
       return;
     }
     var script=document.createElement('script');
-    script.src='js/v6-store-browser.js?v=7.0.0-store-filter.1';
+    script.src='js/v6-store-browser.js?v=7.0.0-live-adapters.1';
     script.async=false;
     script.dataset.digiyarStoreBrowser='1';
     script.onload=function(){ callback(window.DigiYarStoreBrowser); };
