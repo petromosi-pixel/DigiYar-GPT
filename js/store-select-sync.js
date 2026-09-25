59e31edfd8626b835e1168df1a6cd44954e7fb74
petromosi-pixel <petromosi@gmail.com>
2026-09-25 15:25:04 +0330

    Refresh Store Browser bridge to version 20

--- js/store-select-sync.js
+++ js/store-select-sync.js
@@ -25,7 +25,7 @@
       return;
     }
     var script=document.createElement('script');
-    script.src='js/v6-store-browser.js?v=6.0.0-store-browser.19';
+    script.src='js/v6-store-browser.js?v=6.0.0-store-browser.20';
     script.async=false;
     script.dataset.digiyarStoreBrowser='1';
     script.onload=function(){ callback(window.DigiYarStoreBrowser); };
